97dec84dce1a0dcf4090bfab6c48f976b244c9f0
Hirusha Prabhanath <hirushaph@gmail.com>
2024-04-07 23:28:54 +0530

    update template

--- src/utils/template.js
+++ src/utils/template.js
@@ -2,8 +2,8 @@ const singleMovie = function (item) {
   // prettier-ignore
   const msg = `🧧 *${item.title} (${item.year})* \n\n` +
               (item.isAdultRated ? `🔞 *18+* ${item.rated} Rated\n` : "") +
-              " ⭐ ɪᴍᴅʙ : " + item.imdb + "\n" + 
-              (item.rottenTomatoes !== "n/a" ? ` 🍅 ʀᴏᴛᴛᴇɴ ᴛᴏᴍᴀᴛᴏᴇꜱ : ${item.rottenTomatoes}\n` : "") +
+              "⭐ ɪᴍᴅʙ : " + item.imdb + "\n" + 
+              (item.rottenTomatoes !== "n/a" ? `🍅 ʀᴏᴛᴛᴇɴ ᴛᴏᴍᴀᴛᴏᴇꜱ : ${item.rottenTomatoes}\n` : "") +
               ((item.rottenTomatoes === "n/a" && item.imdb === "n/a") ? ` 🌟 Rating : ${item.tmdbRating}\n` : "") +
               "\n🔤 ʟᴀɴɢᴜᴀɢᴇ  : " + item.language + "\n" +
               "🌎 ᴄᴏᴜɴᴛʀʏ    : " + item.country + "\n" +
@@ -20,8 +20,8 @@ const singleTv = function (tv) {
   // prettier-ignore
   const msg = `🎀 *${tv.title} (${tv.year})* \n\n` +
               (tv.isAdultRated ? `🔞 *18+* ${tv.rated} Rated\n` : "") +
-              " ⭐ ɪᴍᴅʙ : " + tv.imdb + "\n" + 
-              (tv.rottenTomatoes !== "n/a" ? ` 🍅 ʀᴏᴛᴛᴇɴ ᴛᴏᴍᴀᴛᴏᴇꜱ : ${tv.rottenTomatoes}\n` : "") +
+              "⭐ ɪᴍᴅʙ : " + tv.imdb + "\n" + 
+              (tv.rottenTomatoes !== "n/a" ? `🍅 ʀᴏᴛᴛᴇɴ ᴛᴏᴍᴀᴛᴏᴇꜱ : ${tv.rottenTomatoes}\n` : "") +
               ((tv.rottenTomatoes === "n/a" && tv.imdb === "n/a") ? ` 🌟 Rating : ${tv.tmdbRating}\n` : "") + 
               "\n📂 ꜱᴇᴀꜱᴏɴꜱ    :  " + tv.seasons + "\n" +
               "🔤 ʟᴀɴɢᴜᴀɢᴇ  :  " + tv.language + "\n" +
