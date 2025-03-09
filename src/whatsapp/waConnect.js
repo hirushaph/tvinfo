dbd025bbeecc5cdf52ccbb5b63d0e8d26fda5b78
Hirusha <157493986+hirushaph@users.noreply.github.com>
2025-03-09 21:45:32 +0530

    setup group meta caching & upgrade packges to latest version

--- src/whatsapp/waConnect.js
+++ src/whatsapp/waConnect.js
@@ -17,7 +17,8 @@ const logger = pino();
 logger.level = "fatal";
 
 //node cache
-const msgRetryCounterCache = new NodeCache();
+// const msgRetryCounterCache = new NodeCache();
+const groupCache = new NodeCache({ stdTTL: 5 * 60, useClones: false });
 
 async function connectToWhatsApp() {
   const { state, saveCreds } = await useMultiFileAuthState("auth_tvinfo");
@@ -33,6 +34,7 @@ async function connectToWhatsApp() {
     },
     logger,
     msgRetryCounterCache,
+    cachedGroupMetadata: async (jid) => groupCache.get(jid),
   });
 
   sock.ev.process(async (events) => {
@@ -58,6 +60,15 @@ async function connectToWhatsApp() {
     }
   });
 
+  sock.ev.on("groups.update", async ([event]) => {
+    const metadata = await sock.groupMetadata(event.id);
+    groupCache.set(event.id, metadata);
+  });
+
+  sock.ev.on("group-participants.update", async (event) => {
+    const metadata = await sock.groupMetadata(event.id);
+    groupCache.set(event.id, metadata);
+  });
   // Handle Messages
   messageHandler(sock);
 
